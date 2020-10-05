f1f5c46fd91ede233d54e853773093d769922578
Damir <damir9715@gmail.com>
2020-10-05 15:16:42 +0600

    initial commit

---
+++ app/src/main/java/com/example/newsapp/api/Network.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.util.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Network {
+
+    private val retrofit by lazy {
+        val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    val api by lazy {
+        retrofit.create(NewsApi::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/util/Conststants.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.util
+
+
+const val API_KEY: String = "fda934fb6fca48d89e4c272b54eba9c3"
+const val BASE_URL: String = "https://newsapi.org"
